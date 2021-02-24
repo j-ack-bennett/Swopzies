2eb9bd15477bb231a36f7185d7c57c71ce451b0c
student <student@eda-bc-03.localdomain>
2021-02-25 10:42:06 +1300

    finish set up

---
+++ client/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { connect } from "react-redux"
+
+function Profile() {
+  return (
+    <div className="container">
+
+    </div>
+    )
+}
+
+export default connect()(Profile)
