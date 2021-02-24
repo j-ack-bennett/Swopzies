ed6f6ca3a4295155b9b609803e2e082f90360800
Rob <robsc801@gmail.com>
2021-02-25 11:57:19 +1300

    working on Nav component

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,5 +1,35 @@
 import React from 'react'
 import { Link }from 'react-router-dom'
 import { logOff } from 'authenticare/client'
+import { connect } from 'react-redux'
+
+const Nav = () => {
+  return (
+      <nav className='navBar'>
+        <div className="container">
+          <div>
+            <h1>Jack Of All Trades</h1>
+          </div>
+          <div>
+            { auth.isAuthenticated
+            ? (<>
+            )}
+          </div>
+        </div>
+
+
+      </nav>
+  )
+  
+
+}
+
+const mapStateToProps = ({auth}) => {
+  return {
+    auth
+  }
+}
+
+export default connect(mapStateToProps)(Nav)
 
 
