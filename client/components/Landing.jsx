9a2c7423a8215ec2e3692312b3e393bd27961bf5
Lara Hayward <22077219+haywardlara@users.noreply.github.com>
2021-02-25 14:31:17 +1300

    landing page

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -4,8 +4,13 @@ import { connect } from "react-redux"
 function Landing() {
   return (
     <div className="container">
-      <p>Landing</p>
-
+      <h1 className="title">Welcome!</h1>
+      <p>Need something fixed? Have the skills to fix something? Want to help someone in your community?</p>
+      <br></br>
+      <p>Jack Of All Trades is a free to use website for people looking to trade goods and services without the price tag!</p>
+      <br></br>
+      <h2 className="subtitle">Register. Post. Trade.</h2>
+      <p>It's that simple!</p>
     </div>
     )
 }
