38e629e54bb5be1cf6752a3f2714071122ef4a00
Jack Bennett <73858124+j-ack-bennett@users.noreply.github.com>
2021-02-26 10:22:57 +1300

    added login styling

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,10 +1,11 @@
-import React, {useState} from "react"
-import {connect} from "react-redux"
+import React, { useState } from "react"
+import { connect } from "react-redux"
+import { Link } from "react-router-dom"
 
-import {loginUser, loginError} from "../actions/auth"
+import { loginUser, loginError } from "../actions/auth"
 
-function Login (props) {
-  const {auth} = props
+function Login(props) {
+  const { auth } = props
 
   const [formData, setFormData] = useState({
     username: "",
@@ -31,27 +32,91 @@ function Login (props) {
   }
 
   return (
-    <form className="form box" onSubmit={handleSubmit}>
-      <h1 className="title is-2">Login</h1>
-      <hr />
-      {auth.errorMessage && (
+    <section className="hero is-success is-fullheight">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <div className="column is-4 is-offset-4">
+            <h3 className="has-text-black">Login</h3>
+            <hr className="login-hr" />
+            <p className="subtitle has-text-black">Please login to proceed.</p>
+            <div className="box">
+
+              <form onSubmit={handleSubmit}>
+                <div className="field">
+                  <div className="control">
+                    <input
+                      required
+                      className="input is-large"
+                      placeholder="Username"
+                      type="text"
+                      name="username"
+                      autoComplete="username"
+                      value={formData.username}
+                      onChange={handleChange}
+                      autoFocus
+                    />
+                  </div>
+                </div>
+
+                <div className="field">
+                  <div className="control">
+                    <input
+                      required
+                      className="input is-large"
+                      placeholder="Password"
+                      type="password"
+                      name="password"
+                      autoComplete="current-password"
+                      value={formData.password}
+                      onChange={handleChange}
+                      autoFocus
+                    />
+                  </div>
+                </div>
+                <button className="button is-block is-info is-large is-fullwidth">Login <i className="fa fa-sign-in" aria-hidden="true"></i></button>
+              </form>
+            </div>
+            <p className="has-text-grey">
+              <Link to="/register">Register</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+const mapStateToProps = ({ auth }) => {
+  return {
+    auth,
+  }
+}
+
+export default connect(mapStateToProps)(Login)
+
+{/* <form className="form box" onSubmit={handleSubmit}>
+    <h1 className="title is-2">Login</h1>
+    <hr />
+    {
+      auth.errorMessage && (
         <span className="has-text-danger is-large">{auth.errorMessage}</span>
-      )}
+      )
+    }
+    < label className = "label is-large has-text-centered" >
+      Username
+      < input
+  required
+  className = "input has-text-centered is-large is-fullwidth"
+  placeholder = "User Name"
+  type = "text"
+  name = "username"
+  autoComplete = "username"
+  value = { formData.username }
+  onChange = { handleChange }
+    />
+      </label >
       <label className="label is-large has-text-centered">
-        Username
-        <input
-          required
-          className="input has-text-centered is-large is-fullwidth"
-          placeholder="User Name"
-          type="text"
-          name="username"
-          autoComplete="username"
-          value={formData.username}
-          onChange={handleChange}
-        />
-      </label>
-      <label className="label is-large has-text-centered">
-        Password
+      Password
         <input
           required
           className="input has-text-centered is-large is-fullwidth"
@@ -68,14 +133,4 @@ function Login (props) {
         value="Login"
         type="submit"
       />
-    </form>
-  )
-}
-
-const mapStateToProps = ({auth}) => {
-  return {
-    auth,
-  }
-}
-
-export default connect(mapStateToProps)(Login)
+    </form > */}
