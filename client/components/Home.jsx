62da4bb5ff96f0beb0c78cf050e7f9af027c56da
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-02-25 15:11:41 +1300

    Home page: bare bones edition with h1 only

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react"
 import { connect } from "react-redux"
+import Listings from './Listings'
 
 function Home() {
+
+  
   return (
     <div className="container">
-      <p>Home</p>
-
+      <div>
+      <h1>I'm looking for. . .</h1>
+      </div>
+      
+      <div>
+      <h1>I can offer. . . </h1>
+      </div>
     </div>
     )
 }
