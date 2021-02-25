8aaceaae15b359888647829ac445fb915accd8ac
naomi-porter <nomyis@hotmail.com>
2021-02-26 10:52:22 +1300

    Back End: DB/Listings updated to include addNewListingTag function, and Routes/Listings POST route updated.

--- server/routes/listings.js
+++ server/routes/listings.js
@@ -1,31 +1,34 @@
 const express = require("express")
 const db = require("../db/listings")
 const router = express.Router()
-const { addNewListing, getListingById, deleteById, updateListing } = require("../db/listings")
+const { addNewListing, addNewListingTag, getListingById, deleteById, updateListing } = require("../db/listings")
 
 module.exports = router
 
 //Get all listings
-router.get("/", (req,res) => {
+router.get("/", (req, res) => {
   db.getListings()
     .then((listings) => {
       res.json(listings)
     })
     .catch((err) => {
-    console.log(err)
-    res.status(500).json({ message: 'Something went wrong'})
-  })
+      console.log(err)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
 })
 
 router.post("/", (req, res) => {
   // let newListing = {user_id: null, type: req.body.type, title: req.body.title, description: req.body.description, image: "", time: null}
-  const newListing = req.body
-  console.log(newListing)
-  addNewListing(newListing)
-    .then((listing) => {
-      // res.json(listing)
-      res.sendStatus(200)
-      return null
+  const newListing = req.body.listing
+  const tagId = req.body.tag.tagId
+  return addNewListing(newListing)
+    .then(listingId => {
+      console.log(listingId)
+      addNewListingTag(addNewListing, tagId)
+        .then(() => {
+          res.sendStatus(200)
+          return null
+        })
     })
 })
 
@@ -40,16 +43,16 @@ router.get("/:id", (req, res) => {
 router.delete("/:id", (req, res) => {
   const id = req.params.id;
   deleteById(id)
-  // console.log(id)
+    // console.log(id)
     .then(() => {
       res.json({})
     })
 })
 
-router.patch ("/:id", (req, res) => {
+router.patch("/:id", (req, res) => {
   const id = req.params.id
-    updateListing(id, req.body)
-      .then((listing) => {
-        res.json(listing)
-      })
+  updateListing(id, req.body)
+    .then((listing) => {
+      res.json(listing)
+    })
 })
