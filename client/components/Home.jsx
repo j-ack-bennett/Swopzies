e31e4df39da5d9993b869aa8e861ccf299f8b346
Jack Bennett <73858124+j-ack-bennett@users.noreply.github.com>
2021-02-26 15:39:19 +1300

    added listings to home page

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,29 +2,52 @@ import React, { useEffect } from "react"
 import { connect } from "react-redux"
 import { fetchListings } from "../actions/listings"
 import { Link } from "react-router-dom"
+import ListingCard from './ListingCard'
 
 function Home(props) {
+
+  const listings = props.listings
+
+
   return (
     <div className="container">
       <div>
-        <Link
+        <h1>I'm Looking For...</h1>
+          {listings.reverse().map(listing => {
+            if(listing.type == "looking"){
+              return <ListingCard key={listing.id} listing={listing} />
+            }
+          })}
+          <Link
           to="/listings"
           onClick={() => localStorage.setItem("type", "looking")}
-        >
-          I'm looking for...
+          >
+          See more...
         </Link>
       </div>
-
+      <hr />
       <div>
-        <Link
+        <h1>I Can Offer...</h1>
+          {listings.map(listing => {
+            if(listing.type == "offer"){
+              return <ListingCard key={listing.id} listing={listing} />
+            }
+          })}
+          <Link
           to="/listings"
           onClick={() => localStorage.setItem("type", "offer")}
-        >
-          I can offer...
+          >
+          See more...
         </Link>
       </div>
     </div>
   )
 }
 
-export default connect()(Home)
+const mapStateToProps = (globalState) => {
+  return {
+    listings: globalState.listings
+  }
+}
+
+export default connect(mapStateToProps)(Home)
