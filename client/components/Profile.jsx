b8e1860657f65f14c88d949ccff61d7bf15ea82b
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-03-01 10:37:26 +1300

    Front End: Profile page, My Listings with titles is rendering, still to work on Edit/Delete functions.

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -3,7 +3,9 @@ import { connect, useStore } from "react-redux";
 
 function Profile(props) {
   const profile = props.auth.user;
-  console.log(props);
+  const listings = props.listings;
+  console.log(listings)
+
   return (
     <div className="card">
       <div className="card-content">
@@ -11,39 +13,59 @@ function Profile(props) {
 
         <div className="content">
           <table className="table-profile">
-           <tbody>
-            <tr>
-              <td>Username</td>
-              <td>{profile.username}</td>
-            </tr>
-            <tr>
-              <td>First Name</td>
-              <td>{profile.first_name}</td>
-            </tr>
-            <tr>
-              <td>Last Name</td>
-              <td>{profile.last_name}</td>
-            </tr>
-            <tr>
-              <td>Email</td>
-              <td>{profile.email}</td>
-            </tr>
-            <tr>
-              <td>Bio</td>
-              <td>{profile.bio}</td>
-            </tr>
-            <tr>
-              <td>Phone</td>
-              <td>{profile.phone}</td>
-            </tr>
-            <tr>
-              <td>Location</td>
-              <td>{profile.location}</td>
-            </tr>
+            <tbody>
+              <tr>
+                <td>Username</td>
+                <td>{profile.username}</td>
+              </tr>
+              <tr>
+                <td>First Name</td>
+                <td>{profile.first_name}</td>
+              </tr>
+              <tr>
+                <td>Last Name</td>
+                <td>{profile.last_name}</td>
+              </tr>
+              <tr>
+                <td>Email</td>
+                <td>{profile.email}</td>
+              </tr>
+              <tr>
+                <td>Bio</td>
+                <td>{profile.bio}</td>
+              </tr>
+              <tr>
+                <td>Phone</td>
+                <td>{profile.phone}</td>
+              </tr>
+              <tr>
+                <td>Location</td>
+                <td>{profile.location}</td>
+              </tr>
             </tbody>
           </table>
         </div>
         <br />
+      </div>
+      <div className="card">
+        <div className="card-content">
+          <h3 className="is-4">My listings</h3>
+          <div className="content">
+            <table className="table-profile">
+              <tbody>
+                {listings.map(listing => {
+                  if (listing.user_id === profile.id) {
+                    return (
+                      <tr key={listing.id}>
+                        <td>{listing.title}</td>
+                      </tr>
+                    )
+                  }
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -52,32 +74,7 @@ function Profile(props) {
 const mapStateToProps = (globalState) => {
   return {
     auth: globalState.auth,
+    listings: globalState.listings,
   };
 };
 export default connect(mapStateToProps)(Profile);
-
-{
-  /* <div className="profileContainer">
-      <div className="fieldName">
-        <div>Username</div>
-        <div>First Name</div>
-        <div>Last Name</div>
-        <div>Email</div>
-        <div>Bio</div>
-        <div>Phone</div>
-        <div>Location</div>
-      </div>
-      <div className="profileDetails">
-        <div>{profile.username}</div>
-        <div>{profile.first_name}</div>
-        <div>{profile.last_name}</div>
-        <div>{profile.email}</div>
-        <div>{profile.bio}</div>
-        <div>{profile.phone}</div>
-        <div>{profile.location}</div>
-      </div>
-      <div className="profileButton">
-        <button>Edit</button>
-      </div>
-    </div> */
-}
