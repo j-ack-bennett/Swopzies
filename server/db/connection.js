db4c3b275fe20235c980e8c985d0224cf6c64011
Lara Hayward <22077219+haywardlara@users.noreply.github.com>
2021-03-01 16:14:49 +1300

    Fix package.json

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,8 +1,6 @@
 const knex = require("knex")
 const config = require("./knexfile")
 const env = process.env.NODE_ENV || "development"
-console.log(env)
-console.log(config[env])
 const connection = knex(config[env])
 
 module.exports = connection
