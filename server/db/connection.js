56a211ba9efdf63f46e24920b4cbc9e2267eea6f
Lara Hayward <22077219+haywardlara@users.noreply.github.com>
2021-03-01 16:07:44 +1300

    con

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,8 @@
-const knex = require('knex')
-const config = require('./knexfile')
-const env = process.env.NODE_ENV || 'development'
+const knex = require("knex")
+const config = require("./knexfile")
+const env = process.env.NODE_ENV || "development"
+console.log(env)
+console.log(config[env])
 const connection = knex(config[env])
 
 module.exports = connection
