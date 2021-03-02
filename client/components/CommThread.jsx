c5dd8cc01068a8d8d7ccd93ae73624649e94e690
Lara Hayward <22077219+haywardlara@users.noreply.github.com>
2021-03-03 09:58:42 +1300

    comments fixed

--- client/components/CommThread.jsx
+++ client/components/CommThread.jsx
@@ -41,14 +41,16 @@ const CommThread = (props) => {
   }
 
   const handleSubmit = (e, threadId) => {
+    e.preventDefault()
     const newComm = {
       listing_id: listingId,
       user_id: user_id,
       text: comment,
       thread_id: threadId,
-      time: new Date(),
+      time: new Date()
     }
     postToThread(newComm)
+    window.location.reload()
   }
 
   return (
