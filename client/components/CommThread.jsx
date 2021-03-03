0c7d66a7e8b94ace390c46cf8a025adc5292271d
Jack Bennett <73858124+j-ack-bennett@users.noreply.github.com>
2021-03-03 16:36:59 +1300

    bren fixed a bren issue

--- client/components/CommThread.jsx
+++ client/components/CommThread.jsx
@@ -54,7 +54,8 @@ const CommThread = (props) => {
   }
 
   return (
-    <div className="margin-bottom margin-top">
+    <div className="margin-bottom margin-top comment-thread">
+      {listing_user_id == user_id ? (
         <>
           {Object.keys(comments).map((key) => {
             const flower = key
@@ -62,10 +63,10 @@ const CommThread = (props) => {
               <div key={flower}>
                 <div key={key}>
                   {comments[key].map((comment) => {
-                    const extraClass = comment.user_id == props.auth.user.id ? '' : ' message-thread--right'
+                    const extraClass = comment.user_id == props.auth.user.id ? ' message-thread--right' : ''
                     return (
-                      <div className={`message-thread${extraClass}`}>
-                        <p key={comment.id}>
+                      <div className={`message-thread${extraClass}`} key={comment.id}>
+                        <p>
                           {comment.username}: {comment.text}
                         </p>
                       </div>
@@ -91,6 +92,47 @@ const CommThread = (props) => {
             )
           })}
         </>
+      ) : (
+        <>
+          {Object.keys(comments).map((key) => {
+            const flower = key
+            return (
+              <div key={flower}>
+                {comments[key][0].user_id == props.auth.user.id ? (
+                  <div key={key}>
+                    {comments[key].map((comment) => {
+                      const extraClass = comment.user_id == props.auth.user.id ? ' message-thread--right' : ''
+                      return (
+                        <div className={`message-thread${extraClass}`} key={comment.id}>
+                          <p>
+                            {comment.username}: {comment.text}
+                          </p>
+                      </div>
+                      )
+                    })}{" "}
+                    <form onSubmit={(e) => handleSubmit(e, flower)}>
+                      <label>
+                      <textarea
+                        className="textarea is-large margin-top"
+                        type="text" 
+                        onChange={handleChange} 
+                        name="text" 
+                        required
+                      />
+                      </label>
+                      <div className="buttons has-addons">
+                        <input className="button is-primary is-fullwidth" type="submit" value="Reply" />
+                      </div>
+                    </form>{" "}
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
+            )
+          })}
+        </>
+      )}
     </div>
   )
 }
